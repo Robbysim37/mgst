c9db815e0f3b89f16b2b338bc39951254d8d7d22
Robby Lewis <Robbysim37@gmail.com>
2023-06-15 15:05:14 -0400

    added more to modal

--- src/components/common/CreateStudentsModal.tsx
+++ src/components/common/CreateStudentsModal.tsx
@@ -1,5 +1,6 @@
-import { Box } from "@mui/material"
+import { Box,Typography } from "@mui/material"
 import ReactDOM from "react-dom"
+import InfoCard from "../styledComponents/InfoCard"
 
 const modalBG = {
     position:"fixed",
@@ -8,13 +9,22 @@ const modalBG = {
     left:0,
     bottom:0,
     right:0,
-    zIndex:100000
+    zIndex:100000,
+    display:"flex",
+    justifyContent:"space-evenly",
+    alignItems:"center",
+    textAlign:"center"
 }
 
 export default function CreateStudentsModal() {
   return ReactDOM.createPortal(
     <Box sx={modalBG}>
-
+        <InfoCard></InfoCard>
+        <Typography sx={{color:'white',fontSize:"2rem"}}>
+            - Create student -<br/>
+            or<br/>
+            - upload student list -</Typography>
+        <InfoCard></InfoCard>
     </Box>,
     document.getElementById("portal")!
   )
---
+++ src/components/styledComponents/InfoCard.tsx
@@ -0,0 +1,13 @@
+import { Card } from "@mui/material"
+
+export default function InfoCard() {
+  return (
+    <Card sx={{
+        backgroundColor:"white",
+        height:"80%",
+        width:"25%",
+    }}>
+
+    </Card>
+  )
+}
