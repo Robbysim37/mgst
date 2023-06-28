b7e7824cf3e1446512d0602d39ab4a3ea5e940ac
Robby Lewis <Robbysim37@gmail.com>
2023-06-27 23:56:18 -0400

    student info can ship to backend

--- src/components/styledComponents/AddStudentCard.tsx
+++ src/components/styledComponents/AddStudentCard.tsx
@@ -1,6 +1,6 @@
 import { Card, FormLabel, FormGroup, 
   FormControl, Input, InputLabel, Stack } from "@mui/material"
-
+import {useState} from "react"
 import StudentSubmit from "./StudentSubmit"
 
 const labelStyles = {fontSize:"2rem",fontFamily:"serif",color:"black"}
@@ -11,6 +11,36 @@ interface Props {
 }
 
 const AddStudentCard:React.FC<Props> = (props) => {
+
+  const [newStudentInfo,setNewStudentInfo] = useState(
+    {
+      firstName: "",
+      lastName: "",
+      cohort: new Date().getFullYear()
+    }
+  )
+
+  const studentInfoChangeHandler = (e:React.ChangeEvent<HTMLInputElement>) => {
+    const regex = /^[0-9\b]+$/
+    if(e.target.id === "cohort"){
+      if (regex.test(e.target.value)) {
+        setNewStudentInfo(
+          {
+            ...newStudentInfo,
+            cohort:parseInt(e.target.value)
+          }
+        );
+      }
+    }else{
+      setNewStudentInfo(
+        {
+          ...newStudentInfo,
+          [e.target.id]:e.target.value
+        })
+        console.log(newStudentInfo)
+    }
+  }
+
   return (
     <Card sx={{
         backgroundColor:"white",
@@ -22,18 +52,18 @@ const AddStudentCard:React.FC<Props> = (props) => {
           <Stack sx={{width:"75%",alignSelf:"center",display:"flex",alignItems:"center"}}>
             <FormControl>
               <InputLabel>First name</InputLabel>
-              <Input sx={inputStyles} autoFocus/>
+              <Input id={"firstName"} onChange={studentInfoChangeHandler} sx={inputStyles} autoFocus/>
             </FormControl>
             <FormControl>
               <InputLabel>Last name</InputLabel>
-              <Input sx={inputStyles}/>
+              <Input id={"lastName"} onChange={studentInfoChangeHandler} sx={inputStyles}/>
             </FormControl>
             <FormControl>
               <InputLabel>Cohort</InputLabel>
-              <Input sx={inputStyles}/>
+              <Input type="number" value={newStudentInfo.cohort} id={"cohort"} onChange={studentInfoChangeHandler} sx={inputStyles}/>
             </FormControl>
 
-            <StudentSubmit setStudentModal={props.setStudentModal}/>
+            <StudentSubmit  newStudentInfoArray={[newStudentInfo]} setStudentModal={props.setStudentModal}/>
             </Stack>
 
         </FormGroup>
