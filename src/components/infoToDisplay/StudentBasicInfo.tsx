95ea3474a6f0430ad7231d03841c81d76b6528fa
Robby Lewis <Robbysim37@gmail.com>
2023-07-13 19:28:41 -0400

    can update students, needs stylings and navigation

--- src/components/infoToDisplay/StudentBasicInfo.tsx
+++ src/components/infoToDisplay/StudentBasicInfo.tsx
@@ -5,6 +5,7 @@ import StudentBasicInfoReadOnly from "./studentDetails/StudentBasicInfoReadOnly"
 import StudentBasicInfoEdit from "./studentDetails/StudentBasicInfoEdit"
 
 interface Props {
+    
     children?: React.ReactNode
     student?: Student
 }
@@ -16,7 +17,7 @@ const StudentBasicInfo:React.FC<Props> = (props) => {
   return (
     <Stack height="100%" display={"flex"} justifyContent={"space-between"} alignItems={"center"}>
       {!editView && <StudentBasicInfoReadOnly student={props.student} setEditView={setEditView}/>}
-      {editView && <StudentBasicInfoEdit student={props.student}/>}
+      {editView && <StudentBasicInfoEdit setEditView={setEditView} student={props.student}/>}
       <Box height={"30%"} width={"80%"} marginBottom={"1rem"}>
         <TextField label={"Notes"} rows={6} multiline sx={{height:"100%",width:"100%"}}>
 
--- src/components/infoToDisplay/studentDetails/StudentBasicInfoEdit.tsx
+++ src/components/infoToDisplay/studentDetails/StudentBasicInfoEdit.tsx
@@ -1,23 +1,63 @@
 import { Student } from "../../../typeScriptDataTypes"
-import {Stack,TextField} from "@mui/material"
+import {Stack,TextField,Button} from "@mui/material"
+import axios from "axios"
+import {useState} from "react"
 
 interface Props {
+    setEditView:Function
     student?:Student
 }
 
 const StudentBasicInfoEdit:React.FC<Props> = (props) => {
 
+  const [studentForm,setStudentForm] = useState({
+    firstName: props.student?.firstName,
+    lastName: props.student?.lastName,
+    cohort: props.student?.cohort
+  })
+
+  const studentFormChangeHandler = (e:React.ChangeEvent<HTMLInputElement>) => {
+    setStudentForm({
+      ...studentForm,
+      [e.target.id]:e.target.value
+    })
+    let test = studentForm
+    console.log(test)
+  }
+
+  const closeFormClickHandler = (e:React.MouseEvent<HTMLButtonElement>) => {
+    props.setEditView(false)
+  }
+
+  const submitFormClickHandler = (e:React.MouseEvent<HTMLButtonElement>) => {
+    axios.put('http://localhost:8000/editStudentInfo',{
+      username:props.student?.username,
+      firstName:studentForm.firstName,
+      lastName:studentForm.lastName,
+      cohort:studentForm.cohort,
+    }).then(
+      props.setEditView(false)
+    )
+  }
+
   return (
     <Stack>
-    <TextField variant="filled">
+    <TextField variant="filled" onChange={studentFormChangeHandler} 
+    id="firstName" value={studentForm.firstName}>
       {props.student?.firstName} {props.student?.lastName}
     </TextField>
-    <TextField variant="filled">
-      Grade: {props.student?.grade}
+    <TextField variant="filled" onChange={studentFormChangeHandler} 
+    id="lastName" value={studentForm.lastName}>
+      {props.student?.firstName} {props.student?.lastName}
     </TextField>
-    <TextField variant="filled">
+    <TextField variant="filled" onChange={studentFormChangeHandler} 
+    id="cohort" value={studentForm.cohort}>
       Cohort: {props.student?.cohort}
     </TextField>
+    <Stack direction={"row"}>
+      <Button onClick={closeFormClickHandler}>Close</Button>
+      <Button onClick={submitFormClickHandler}>Submit</Button>
+    </Stack>
 </Stack>
   )
 }
