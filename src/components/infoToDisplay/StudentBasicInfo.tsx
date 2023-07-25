c96cf94f3c8cc4c3e7a7f891306d621a7b1b9ee1
Robby Lewis <Robbysim37@gmail.com>
2023-07-25 15:00:49 -0400

    students can be deleted from app, needs factoring

--- src/components/infoToDisplay/StudentBasicInfo.tsx
+++ src/components/infoToDisplay/StudentBasicInfo.tsx
@@ -1,8 +1,12 @@
-import {Stack,TextField,Box} from "@mui/material"
+import {Stack,TextField,Box,IconButton,Menu,MenuItem} from "@mui/material"
 import { Student } from "../../typeScriptDataTypes"
 import { useState } from "react"
 import StudentBasicInfoReadOnly from "./studentDetails/StudentBasicInfoReadOnly"
 import StudentBasicInfoEdit from "./studentDetails/StudentBasicInfoEdit"
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import SettingsIcon from '@mui/icons-material/Settings';
+import axios from "axios"
+import { useNavigate } from "react-router-dom"
 
 interface Props {
     
@@ -13,9 +17,35 @@ interface Props {
 const StudentBasicInfo:React.FC<Props> = (props) => {
 
   const [editView,setEditView] = useState(false)
+  const [anchorEl,setAnchorEl] = useState<null | HTMLElement>(null)
+  const open = Boolean(anchorEl)
+  const navigate = useNavigate()
+
+  const studentMenuClickHandler = (e:React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(e.currentTarget)
+  }
+
+  const deleteStudentClickHandler = (e:React.MouseEvent<HTMLLIElement>) => {
+    axios.delete('http://localhost:8000/deleteStudent',{data:{username:props.student.username}}).then(res => {
+      navigate("/staff")
+    }).catch(response => {
+      console.log("fail")
+    })
+  }
 
   return (
-    <Stack height="100%" display={"flex"} justifyContent={"space-between"} alignItems={"center"}>
+    <Stack height="100%" display={"flex"} alignItems={"center"}>
+      <Menu anchorEl={anchorEl} open={open} id={"student-menu"}>
+        <MenuItem onClick={deleteStudentClickHandler} sx={{color:"red"}}>Delete Student</MenuItem>
+      </Menu>
+      <Stack direction={"row"} justifyContent={"space-between"} width={"90%"} height={"15%"}>
+        <IconButton>
+          <ArrowBackIcon/>
+        </IconButton>
+        <IconButton id={"student-menu"} onClick={studentMenuClickHandler}>
+          <SettingsIcon/>
+        </IconButton>
+      </Stack>
       {!editView && <StudentBasicInfoReadOnly student={props.student} setEditView={setEditView}/>}
       {editView && <StudentBasicInfoEdit setEditView={setEditView} student={props.student}/>}
       <Box height={"30%"} width={"80%"} marginBottom={"1rem"}>
