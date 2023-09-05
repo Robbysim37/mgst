f95aad8136ef6ce8dc275e8a6e5997ca96313df6
Robby Lewis <Robbysim37@gmail.com>
2023-09-05 14:17:36 -0400

    tokens

--- src/utils/PrivateRoutes.tsx
+++ src/utils/PrivateRoutes.tsx
@@ -1,8 +1,22 @@
 import { Outlet, Navigate } from "react-router-dom";
-import { useState } from "react";
+import { useState,useEffect } from "react";
+import axios from "axios"
 
 const PrivateRoutes = () => {
-    const [auth,setAuth] = useState(sessionStorage.getItem("userName"))
+    const [auth,setAuth] = useState(true)
+
+    // useEffect(() => {
+    //     const sessionToken = window.sessionStorage.getItem("token")
+    //     const sessionUser = window.sessionStorage.getItem("user")
+    //     if(!sessionToken || !sessionUser){
+    //         setAuth(false)
+    //     }else{
+    //         axios.post("http://localhost:3000/checkToken",{username:sessionUser,token:sessionToken})
+    //         .then(promise => {
+    //             // if promise.data
+    //         })
+    //     }
+    // },[])
 
     return(
         auth ? <Outlet/> : <Navigate to="/"/>
