a8232f3c3d1dc914b954977eac93a0a15a2f1a7b
Robby Lewis <Robbysim37@gmail.com>
2023-09-25 13:58:38 -0400

    finished

--- src/components/infoDisplayers/InfoCard.tsx
+++ src/components/infoDisplayers/InfoCard.tsx
@@ -1,10 +1,13 @@
 import { Card } from "@mui/material";
 import {Student} from "../../typeScriptDataTypes"
 import { useNavigate } from "react-router-dom";
+import "../../hidden-scroll-wheel.css"
 
 interface Props {
     children?: React.ReactNode;
     color?: string;
+    justifyContent?:string;
+    alignItems?:string;
     student?: Student;
     height?: string;
     width?: string;
@@ -31,10 +34,13 @@ const InfoCard: React.FC<Props> = (props) => {
         <Card
         sx={{
         backgroundColor:props.color ||"white",
+        justifyContent:props.justifyContent || "flex-start",
+        alignItems:props.alignItems || "stretch",
         display:props.display,
         height:props.height || "50%",
         width:props.width || "50%",
         margin:"2.5%",
+        overflow:"scroll",
         borderRadius:"30px"}}
         onClick={cardClickHandler}>
             {props.children}
--- src/components/infoToDisplay/GetStaffList.tsx
+++ src/components/infoToDisplay/GetStaffList.tsx
@@ -1,17 +1,20 @@
-import {Stack,Typography} from "@mui/material"
+import {Stack} from "@mui/material"
 import StaffCard from "../infoDisplayers/StaffCard"
 
 
 interface Props {
-  staff:[string] | null
+  staff:[string] | null,
+  setIsLoading:Function,
+  setRemoveStaffModal:Function
 }
 
 const GetStaffList:React.FC<Props> = (props) =>  {
 
   return (
-    <Stack height={"100%"} width={"100%"} alignItems={"center"} spacing={2}>
+    <Stack bgcolor={"primary.light"} height={"80%"} width={"100%"} alignItems={"center"} spacing={2}>
       {props.staff !== null && props.staff.map( currStaff => {
-        return <StaffCard key={Math.random()}>{currStaff}</StaffCard>
+        return <StaffCard setRemoveStaffModal={props.setRemoveStaffModal}
+        setIsLoading={props.setIsLoading} key={Math.random()}>{currStaff}</StaffCard>
       }
       )}
     </Stack>
